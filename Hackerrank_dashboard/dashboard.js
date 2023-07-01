ea4742fe8ff7f5276fb1ab8dcfa8281cb4e911e9
LAKSH DOSHI <102138784+LAKSH3002@users.noreply.github.com>
2023-07-01 22:45:02 +0530

    database project started

---
+++ Hackerrank_dashboard/dashboard.js
@@ -0,0 +1,3 @@
+// flutter app with database connection.
+// Next project should be tours and travel website with database connectivity.
+// Then airline booking system using java swing and database connectivity.
